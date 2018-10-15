b82a13beb5a33e022e750315e50b85dbb0312d5b
wallacecantible <43582950+wallacecantible@users.noreply.github.com>
2018-10-14 23:03:04 -0400

    Add files via upload

---
+++ one.js
@@ -0,0 +1,24 @@
+function uone() {
+    var x = document.getElementById("uone");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function utwo() {
+    var x = document.getElementById("utwo");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function uthree() {
+    var x = document.getElementById("uthree");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
