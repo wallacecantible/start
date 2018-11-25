d72ed90ca6822e2ede1d8b4315717aebea620900
wallacecantible <43582950+wallacecantible@users.noreply.github.com>
2018-11-24 20:54:21 -0500

    Add files via upload

--- one.js
+++ one.js
@@ -38,3 +38,11 @@ function ufive() {
         x.style.display = "none";
     }
 }
+function usix() {
+    var x = document.getElementById("usix");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
